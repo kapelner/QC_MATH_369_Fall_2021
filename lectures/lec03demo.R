7254356681c601dcb5c109b3676bee1b2343e293
Adam Kapelner <kapelner@gmail.com>
2021-09-02 19:47:22 -0400

    lec 3 notes

---
+++ lectures/lec03demo.R
@@ -0,0 +1,15 @@
+pacman::p_load(ggplot2)
+
+x = 0:20
+probs = dbinom(x, 20, 0.524)
+ggplot(data.frame(y = probs, x = x)) + 
+  geom_bar(aes(x = x, y = probs), stat = 'identity') +
+  ylab("binomial probability")
+
+ret_prob = sum(probs[7:14])
+ret_prob
+1 - ret_prob
+
+ret_prob = sum(probs[6:15])
+ret_prob
+1 - ret_prob
